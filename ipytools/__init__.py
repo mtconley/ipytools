23824f156a19dac102aa7e9b41395d86f0ed8f6e
Matthew Conley <matthewt.conley@gmail.com>
2016-01-21 14:00:17 -0800

    update __init__ to import additions

--- ipytools/__init__.py
+++ ipytools/__init__.py
@@ -1,1 +1,9 @@
-from _core import Suppress, Redirect, Timer, slide, mplrc, hdisplay, get_classname
+from _core import (Suppress, 
+                   Redirect, 
+                   Timer, 
+                   slide, 
+                   mplrc, 
+                   hdisplay, 
+                   get_classname)
+
+from injections import (toggle_input_cell)
